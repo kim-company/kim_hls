9c4897c2237dbf233bce2fbc034b78a611bcc19f
Philip Giuliani <philip.giuliani@keepinmind.info>
2025-11-03 16:27:43 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule HLS.MixProject do
   def project do
     [
       app: :kim_hls,
-      version: "2.5.10",
+      version: "2.5.11",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
